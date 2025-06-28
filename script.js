c14840fd137d116a6979aee38109ab3ed0c6dca3
MUHAMMAD ZAHIRUL <jibonsarker228@gmail.com>
2025-06-28 14:45:26 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-javascript
-function handleDownload() {
-  alert("অ্যাপ ডাউনলোড লিংক খুব শীঘ্রই যুক্ত হবে। আপাতত ধন্যবাদ!");
-}
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("FireArena BD loaded successfully!");
+});
